a0cc58bbdf0e2723dfed46e195986c2aa2f07929
SovaBob <malahov_mirnaya22@mail.ru>
2019-12-27 23:15:25 +0300

    Add Constructor

--- BinTree/BinTree.Tests/BinTreeTest.cs
+++ BinTree/BinTree.Tests/BinTreeTest.cs
@@ -3,5 +3,20 @@ using BinTree;
 
 namespace BinTree.Tests
 {
-    
+    [TestFixture]
+    public class BinTreeTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(1)]
+        public void TestConstructor(int data)
+        {
+            BinTree t = new BinTree(data);
+            Assert.AreEqual(data, t.origin.data);
+        }
+    }
 }
--- BinTree/BinTree/BinTree.cs
+++ BinTree/BinTree/BinTree.cs
@@ -6,5 +6,22 @@ using System.Threading.Tasks;
 
 namespace BinTree
 {
-    
+    public class BinTree
+    {
+        public class Vertex
+        {
+            public int data;
+            public Vertex left = null;
+            public Vertex right = null;
+            public Vertex(int data)
+            {
+                this.data = data;
+            }
+        }
+        public Vertex origin;
+        public BinTree(int data)
+        {
+            origin = new Vertex(data);
+        }
+    }
 }
